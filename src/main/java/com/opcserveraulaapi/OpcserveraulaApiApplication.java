2b74bbd0ba31b2bdde68c553aea944171eb17f31
Genilson de Oliveira Alves <genilson.alves@valgroupco.com>
2023-03-09 16:39:41 -0400

    startproject

---
+++ src/main/java/com/opcserveraulaapi/OpcserveraulaApiApplication.java
@@ -0,0 +1,14 @@
+package com.opcserveraulaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcserveraulaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcserveraulaApiApplication.class, args);
+		System.out.println("Server UP QUE COISA LINDA!");
+	}
+
+}
---
+++ src/test/java/com/opcserveraulaapi/OpcserveraulaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opcserveraulaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcserveraulaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
